9a2fb410d03806e83ce450d0f13578499e168f5b
Pyrenam <pyrenam@gmail.comecho>
2021-03-31 10:34:58 +0200

    [echo.js]

--- echo.js
+++ echo.js
@@ -1,7 +1,11 @@
-let tab = process.argv
+//    Affiche à l'écran les strings passées en arguments.
+//    node echo.js hello hard fork
+//    hello hard fork
+
 let result = ''
 
-for (const elem of tab){
-  result +='$(elem)'
+for (let i = 2; i < process.argv.length; ++i ) {   // 
+  result += process.argv[i] + ' '
 }
+
 console.log(result)
